d6134f6652819dc6460c211ac391008df7f89172
larcanus <larcanus@yandex.ru>
2025-06-08 22:59:52 +0300

    add battle log scene and background

---
+++ scenes/page/battle/battle.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+
+func _ready() -> void:
+	on_change_scale();
+
+
+func on_change_scale():
+	get_tree().get_root().size_changed.connect(_on_resize)
+	_on_resize()
+
+
+func _on_resize():
+	var viewport_size = get_viewport_rect().size
+	var texture_size = $Background.texture.get_size()
+	var scale_x = viewport_size.x / texture_size.x
+	var scale_y = viewport_size.y / texture_size.y
+	var target_scale = max(scale_x, scale_y)
+
+	$Background.scale = Vector2(target_scale, target_scale)
+	var position_y = viewport_size.y / 2;
+	var position_x = (viewport_size.x / 5) * 3;
+	$Background.position = Vector2(position_x, position_y)
---
+++ scenes/ui/battle_log.gd
@@ -0,0 +1,42 @@
+extends Control
+
+@onready var log_container = $ScrollContainer/LogContent
+
+func _ready():
+	_update_size()
+	get_tree().root.size_changed.connect(_update_size)
+	connect("resized", _update_margins)
+	# Тест - добавление записей
+	TimerGlobal.add_callback(add_log_message);
+
+func _update_size():
+	var viewport_size = get_viewport_rect().size
+	custom_minimum_size = Vector2(viewport_size.x * 0.25, viewport_size.y)
+	_update_margins()
+	add_log_message('_update_size' + JSON.stringify(viewport_size))
+
+
+func _update_margins():
+	if not is_inside_tree():
+		await ready
+
+	var margin_lef_right_px = self.size * 0.1
+	var margin_top_bottom_px = self.size * 0.05
+	$ScrollContainer.set("offset_left", margin_lef_right_px.x)
+	$ScrollContainer.set("offset_right", -margin_lef_right_px.x)
+	$ScrollContainer.set("offset_top", margin_top_bottom_px.y)
+	$ScrollContainer.set("offset_bottom", -margin_top_bottom_px.y)
+
+	$ScrollContainer.queue_redraw()
+
+
+func add_log_message(text: String = 'log_message'):
+	var label = Label.new()
+	label.text = text
+	label.autowrap_mode = TextServer.AUTOWRAP_WORD_SMART
+	label.horizontal_alignment = HORIZONTAL_ALIGNMENT_LEFT
+	label.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	log_container.add_child(label)
+
+	await get_tree().process_frame
+	$ScrollContainer.scroll_vertical = $ScrollContainer.get_v_scroll_bar().max_value
