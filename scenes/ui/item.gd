3e53153d8f0c94162330325332577c215286f1ec
larcanus <larcanus@yandex.ru>
2025-06-13 19:44:27 +0300

    add animate icon from press

--- scenes/ui/item.gd
+++ scenes/ui/item.gd
@@ -18,6 +18,7 @@ func setup(texture_path: String, text: String = ""):
 
 	# Назначение
 	icon.texture_normal = texture
+	icon.texture_pressed = texture
 
 	# Если есть текст
 	if is_instance_valid(label):
@@ -25,3 +26,30 @@ func setup(texture_path: String, text: String = ""):
 
 	# Настройка размеров
 	self.custom_minimum_size = Vector2(32, 32)  # Базовый размер
+
+
+func _on_icon_pressed() -> void:
+	print('_on_icon_pressed') # Replace with function body.
+	animate_icon_click()
+
+
+
+var is_animating = false
+func animate_icon_click():
+	if is_animating:
+		return
+
+	is_animating = true
+	var tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+	var click_scale = Vector2(0.85, 0.85)
+	var original_pos = icon.position
+	var original_scale = icon.scale
+	var offset = (icon.size * (1.0 - click_scale.x)) / 2
+
+	tween.tween_property(icon, "scale", click_scale, 0.06)
+	tween.parallel().tween_property(icon, "position", original_pos + offset, 0.06)
+
+	tween.tween_property(icon, "scale", original_scale, 0.06)
+	tween.parallel().tween_property(icon, "position", original_pos, 0.06)
+
+	tween.finished.connect(func(): is_animating = false)  # Разблокируем после завершения
